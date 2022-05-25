8990dca35968db8cf154e52c14d1c43f2917c4d3
KazumaTasiro <k021g1450@g.neec.ac.jp>
2022-05-25 11:15:04 +0900

    a

---
+++ Triangle.cpp
@@ -0,0 +1,21 @@
+#include "Triangle.h"
+
+DrawingMesh::DrawingMesh()
+{
+}
+
+DrawingMesh::~DrawingMesh()
+{
+}
+
+void DrawingMesh::init()
+{
+}
+
+void DrawingMesh::Update()
+{
+}
+
+void DrawingMesh::Draw()
+{
+}
---
+++ Triangle.h
@@ -0,0 +1,11 @@
+#pragma once
+class DrawingMesh
+{
+public:
+	DrawingMesh();
+	~DrawingMesh();
+	void init();
+	void Update();
+	void Draw();
+
+};
