d1fd0ab3301b0b1bf1f162285010e4328de3f169
一真 田代 <t.kazuma.0809@gmail.com>
2023-10-18 14:02:33 +0900

    スタートのCppを作った

---
+++ User/SeenTransition/StertCount.cpp
@@ -0,0 +1,40 @@
+#include "StertCount.h"
+
+
+StertCount::StertCount()
+{
+}
+
+StertCount::~StertCount()
+{
+}
+
+void StertCount::Initialize()
+{
+	bossStertModel = Model::LoadFormOBJ("Sakaban");
+	bossStert = new Object3d();
+	bossStert->Initialize();
+	bossStert->SetModel(bossStertModel);
+	bossStert->wtf.scale = { BossScale };
+	bossStert->wtf.position = { StertBossPos };
+}
+
+void StertCount::Update()
+{
+}
+
+void StertCount::Draw()
+{
+	if ( camera_->GetEye().z<=-300 )
+	{
+		bossStert->Draw();
+	}
+}
+
+void StertCount::CameraUpdate()
+{
+}
+
+void StertCount::BossUpdate()
+{
+}
--- User/SeenTransition/StertCount.h
+++ User/SeenTransition/StertCount.h
@@ -1,1 +1,45 @@
 #pragma once
+#include "Object3d.h"
+#include "Model.h"
+
+
+class StertCount
+{
+public:
+	StertCount();
+	~StertCount();
+	///<summary>
+	////初期化
+	///</summary>
+	void Initialize();
+	///<summary>
+	////更新
+	///</summary>
+	void Update();
+	///<summary>
+	////描画
+	///</summary>
+	void Draw();
+	///<summary>
+	////カメラセット
+	///</summary>
+	void SetCamera(Camera* camera) {
+		camera_ = camera;
+	}
+	///<summary>
+	////カメラアップデート
+	///</summary>
+	void CameraUpdate();
+	///<summary>
+	////ボスアップデート
+	///</summary>
+	void BossUpdate();
+private:
+	Camera* camera_ = nullptr;
+	Object3d* bossStert = nullptr;
+	Model* bossStertModel = nullptr;
+	//ボススケール
+	Vector3 BossScale = { 20,20,20 };
+	//ボスポジション
+	Vector3 StertBossPos = { 0,4, -500 };
+};
